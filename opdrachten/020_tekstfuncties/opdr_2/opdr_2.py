cf3b4931dd1e6b53b7f0f04ead55a4fee9c44738
Dior <nomail.mail.invalid>
2024-01-11 09:07:00 +0100

    Opdracht 020_tekstfuncties\opdr_2

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -4,6 +4,7 @@
 
 # Hier komt je code...
 
-my_list = ...
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+count_t = my_string.count("t")
 
-print()
+print(f"De letter \"t\" komt {count_t}x voor in my_string")
